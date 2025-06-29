a323b303673d047eddd3ef413c92ecf26903eadf
szentebalint0 <balint.szente@stud.uni-corvinus.hu>
2025-06-29 16:59:20 +0200

    adding topping prices

--- OOPMasterChallenge/src/Topping.java
+++ OOPMasterChallenge/src/Topping.java
@@ -1,10 +1,15 @@
 public class Topping {
     private String name;
-    private static double price = 0.5;
+    private double price;
 
     public Topping(String name) {
         this.name = name;
-
+        this.price = switch (name.toLowerCase()){
+            case "mayo", "ketchup", "mustard" -> 0.25;
+            case "pickles", "onion", "red onion", "tomato" -> 0.35;
+            case "bacon", "ham", "salami", "chicken", "patty" -> 1.0;
+            default -> 0;
+        };
     }
 
     public double getPrice() {
