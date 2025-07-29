e0520d47d059937cfb120ec014bd262e07c41c1a
szentebalint0 <balint.szente@stud.uni-corvinus.hu>
2025-07-29 19:38:35 +0200

    finish

---
+++ InterfaceChallenge/src/Player.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+public class Player implements ISaveable{
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.name = name;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public List<String> write() {
+        return List.of(this.name, this.weapon, Integer.toString(hitPoints), Integer.toString(strength));
+    }
+
+    @Override
+    public void read(List<String> list) {
+
+        if (list != null && !list.isEmpty()) {
+            list.addAll(List.of(this.name, this.weapon, Integer.toString(hitPoints), Integer.toString(strength)));
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Player{name='" + this.name + "', hitPoints=" + this.hitPoints + ", strength=" + this.strength + " weapon='" + this.weapon +"}";
+    }
+}
